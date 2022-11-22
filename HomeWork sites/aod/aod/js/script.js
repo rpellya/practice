6c1b6c55d11758aa3fa92ce0d41dcbe15e1dd095
rpellya <rpellya@gmail.com>
2022-11-22 20:34:42 +0300

    Дошел до блока слайдера, пока не получилось

--- HomeWork sites/aod/aod/js/script.js
+++ HomeWork sites/aod/aod/js/script.js
@@ -19,3 +19,23 @@ function ibg(){
       $('.menu__body').toggleClass('active');
       $('body').toggleClass('lock');
    });
+
+   //SLIDERS
+   if($('.slider__body').length>0){
+      $('.slider__body').slick({
+         //autoplay: true,
+         //infinite: false,
+         dots: true,
+         arrows: false,
+         accessibility: false,
+         slidesToShow: 1,
+         autoplaySpeed: 3000,
+         adaptiveHeight: true,
+         nextArrow: '<button type="button" class="slick-next"></button>',
+         prevArrow: '<button type="button" class="slick-prev"></button>',
+         repsonsive: [{
+            breakpoint: 768,
+            settings: {}
+         }]
+      });
+   }
